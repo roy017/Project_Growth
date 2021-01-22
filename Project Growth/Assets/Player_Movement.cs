a9e44522fccd82e620c0fc1ed571bcf1b0a3a70d
roy017 <41708735+roy017@users.noreply.github.com>
2021-01-22 19:07:45 +0100

    Simple movement script

---
+++ Project Growth/Assets/Player_Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private float Player_Speed = 3.0f;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        if (movement.magnitude >=  0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f).normalized;
+            
+            controller.Move(movement * Time.deltaTime * Player_Speed);
+        }
+        
+
+        /*
+        if (movement != Vector3.zero)
+        {
+            gameObject.transform.forward = movement;
+        }
+        */
+
+        // Changes the height position of the player..
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Space)) && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
